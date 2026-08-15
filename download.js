e64e9519c5b4645f12a9ade737693e035e7a1c41
duckyduck <specialstrovo@gmail.com>
2026-08-16 02:37:55 +0300

    vibmc page: pin to v0.0.6 (Nether & End, portals, online mode); Nether card + checklist now ember; java 8 across the page

--- download.js
+++ download.js
@@ -2,7 +2,7 @@
   "use strict";
 
   const REPO = "gabytz777/vib-MC";
-  const STABLE_TAG = "v0.0.4-hotfix.3";
+  const STABLE_TAG = "v0.0.6";
   const API = `https://api.github.com/repos/${REPO}/releases/tags/${STABLE_TAG}`;
   const FALLBACK_JAR = `https://github.com/${REPO}/releases/download/${STABLE_TAG}/vib-mc.jar`;
 
